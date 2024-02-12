c6db63ee600bef4e52f9c099d4c0971ac26f1458
ALFAFA <gasan1288@gmail.com>
2024-02-12 04:09:10 +0400

    added curved container bar

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../../../../utils/constants/colors.dart';
+import '../curved_edges/curved_edges_widget.dart';
+import 'circular_container.dart';
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key, required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.only(bottom: 0.0),
+
+        /// -- if size.infinite is not true error occurred
+        child: Stack(
+          children: [
+            /// -- Background Custom Shapes
+            Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(
+                  backgroundColor: TColors.white.withOpacity(0.1),
+                )),
+            Positioned(
+              top: 100,
+              right: -300,
+              child: TCircularContainer(
+                backgroundColor: TColors.white.withOpacity(0.1),
+              ),
+            ),
+            child,
+          ],
+        ),
+      ),
+    );
+  }
+}
