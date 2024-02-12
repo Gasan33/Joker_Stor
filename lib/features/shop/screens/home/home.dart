5931888955b006573636f8c5ff49ed67668e8b7c
ALFAFA <gasan1288@gmail.com>
2024-02-12 22:16:59 +0400

    added appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:new_ecommerce_app/common/widgets/appbar/appbar.dart';
 import 'package:new_ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
-import 'package:new_ecommerce_app/utils/constants/colors.dart';
-import 'package:new_ecommerce_app/utils/constants/text_strings.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../common/widgets/products_cart/cart_menu_icon.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
