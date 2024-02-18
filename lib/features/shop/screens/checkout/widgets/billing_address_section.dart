2110b732c64a721bee4c9ec7304e0d26b174e20c
ALFAFA <gasan1288@gmail.com>
2024-02-18 06:45:00 +0400

    added checkout Screen

---
+++ lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:new_ecommerce_app/common/widgets/texts/section_header.dart';
+import 'package:new_ecommerce_app/utils/constants/sizes.dart';
+class TBillingAddressSection extends StatelessWidget {
+  const TBillingAddressSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TSectionHeading(title: 'Shipping Address',buttonTitle: 'Change',onPressed: (){},),
+        Text('Gasan Mansour',style: Theme.of(context).textTheme.bodyLarge,),
+        const SizedBox(width: TSizes.spaceBtwItems/2,),
+
+        Row(
+          children: [
+            const Icon(Icons.phone,color: Colors.grey,size: 16,),
+            const SizedBox(width: TSizes.spaceBtwItems,),
+            Text('+971-545-671677',style: Theme.of(context).textTheme.bodyLarge,),
+
+          ],
+        ),
+        const SizedBox(width: TSizes.spaceBtwItems/2,),
+
+        Row(
+          children: [
+            const Icon(Icons.location_history,color: Colors.grey,size: 16,),
+            const SizedBox(width: TSizes.spaceBtwItems,),
+            Expanded(child: Text('South Liane, Maine 887695, USA',style: Theme.of(context).textTheme.bodyMedium,softWrap: true,)),
+
+          ],
+        ),
+      ],
+    );
+  }
+}
