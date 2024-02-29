ae10fc7a90112f3eb5c0ac16d23f40fcfe1b675e
ALFAFA <gasan1288@gmail.com>
2024-02-29 20:32:07 +0400

    added login controller

---
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:new_ecommerce_app/data/repositories/authentication_repository/authentication_repository.dart';
+import 'package:new_ecommerce_app/utils/constants/image_strings.dart';
+import 'package:new_ecommerce_app/utils/popups/full_screen_loader.dart';
+
+import '../../../../utils/helpers/network_manager.dart';
+import '../../../../utils/popups/loaders.dart';
+
+class LoginController extends GetxController {
+  /// Variables
+  final rememberMe = false.obs;
+  final hidePassword = true.obs;
+  final localStorage = GetStorage();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+
+  @override
+  void onInit() {
+    //if(localStorage.read('REMEMBER_ME_EMAIL').toString().isNotEmpty&&localStorage.read('REMEMBER_ME_PASSWORD').toString().isEmpty){
+      email.text = localStorage.read('REMEMBER_ME_EMAIL') ?? "";
+      password.text = localStorage.read('REMEMBER_ME_PASSWORD') ?? "";
+
+    super.onInit();
+  }
+
+  /// -- Email and Password SignIn
+  Future<void> emailAndPasswordSignIn() async {
+    try {
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog('Logging you in...', TImage.loader);
+
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        //   TFullScreenLoader.stopLoading();
+        //
+        // }else{
+        return;
+      }
+
+      // Form Validation
+      if (!loginFormKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        //
+        // }else{
+        return;
+      }
+
+      // Save Data if Remember Me is Selected
+      if (rememberMe.value) {
+        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+        localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+      }
+
+      // Login user using EMail & Password Authentication
+      final userCredentials = await AuthenticationRepository.instance
+          .loginWithEmailAndPassword(
+              email.text.trim(), password.text.toString());
+
+
+
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
+
+      // Redirect
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      // Show some Generic Error to the user
+      TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
+  }
+}
